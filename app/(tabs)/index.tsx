2ec47b02d749eeccba5d8f023c6a382ea7165102
JAI RUS MATTHEW LAYOS <jrmdlayos@mymail.mapua.edu.ph>
2024-12-27 17:50:50 +0800

    Baseline push

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,47 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { Pressable, SafeAreaView, Text, View } from 'react-native';
+import { useRouter } from 'expo-router';
+import { Image } from 'expo-image';
+import { Asset } from 'expo-asset';
+import { useFonts } from 'expo-font';
+import './../global.css';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
-export default function HomeScreen() {
+const uploadIcon = Asset.fromModule(require("../../assets/images/upload.png")).uri;
+
+
+export default function Index() {
+  const [fontsLoaded] = useFonts({
+    'Source Sans 3': require('../../assets/fonts/SourceSans3-VariableFont_wght.ttf'), // Make sure the path is correct
+  });
+
+  if (!fontsLoaded) {
+    return <Text>Loading...</Text>; // Or a loading spinner
+  }
+
+  const router = useRouter();
+
+  const next = () => {
+    router.push('/explore');
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <>
+      <StatusBar style="light" />
+      <SafeAreaView className="flex-1 border-2 w-full bg-white items-center">
+        <View className="h-10 border-2 bg-primary_green w-full" />
+        <Pressable className="border-2 border-secondary_nuance w-5/6 mt-5 items-center flex" onPress={next}>
+          <View className='flex-col p-8'>
+            <Image
+              source={{uri:uploadIcon}}
+              style={{ width: 190, height: 170 }}
+              contentFit='cover'
+            />
+            <Text className='text-center font-[Source Sans 3]'>Upload and Clean Dataset</Text>
+          </View>
+        </Pressable>
+      </SafeAreaView>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
